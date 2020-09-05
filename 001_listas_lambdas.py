46eed0f01b22af070c0699267e3995244f5d987e
Romel Alejandro Hernandez Resendiz <romelresndiz@gmail.com>
2020-09-05 10:20:23 -0500

    seguimos aprendiendo mi gente

---
+++ 001_listas_lambdas.py
@@ -0,0 +1,27 @@
+numbers_list = [
+    8,
+    10,
+    23,
+    38,
+    126,
+    128,
+    18,
+    19,
+    22,
+    9,
+
+]
+
+def double_number(value):
+    return value * 2
+
+number = 2500
+number_double=double_number(number)
+print('_________________________')
+print(f'Calculando el doble de {number}: {number_double}\n\n')
+
+
+print('_________________________')
+for n in numbers_list:
+    r=
+print(f'Calculando el doble de {number}: {number_double}\n\n')
