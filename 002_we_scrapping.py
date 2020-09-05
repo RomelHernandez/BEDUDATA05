c02a2a6783b813b4988a419f360765627019e5c4
Romel Alejandro Hernandez Resendiz <romelresndiz@gmail.com>
2020-09-05 11:38:26 -0500

    agregamos scarpping

--- 002_we_scrapping.py
+++ 002_we_scrapping.py
@@ -1,9 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
-
+# Requests to web server to get HTML Plain
 BASE_URL = 'https://realpython.com/'
-
 response = requests.get(BASE_URL)
-response_content = response.content 
+response_content = response.content
+# BS Implementation
+soup = BeautifulSoup(response_content, 'html.parser')
+# print(soup)
+# print(soup.title)
+# print(soup.title.string)
+links = soup.find_all('a')
+print(links)
+print(len(links))
 
-print(response_content)
+links_wrapper = soup.find('div', class_='sidebar-module sidebar-module-inset border')
+print(links_wrapper)
